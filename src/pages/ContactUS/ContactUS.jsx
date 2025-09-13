848bf4c80e6e4b70a00210ebede92d1646383f24
Mohd Husain <mhusain@gmail.com>
2025-09-13 04:20:51 -1000

    finel

--- src/pages/ContactUS/ContactUS.jsx
+++ src/pages/ContactUS/ContactUS.jsx
@@ -78,7 +78,7 @@ const ContactUS = () => {
   const emailMethod = "php";
   const mailerSetting = {
     fromName: "Gokuldas Website",
-    sendTo: ["thecoderashok@gmail.com"],
+    sendTo: ["verligte@gmail.com"],
   };
 
   useEffect(() => {
